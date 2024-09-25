49f5cc186121ab43044db75a19a27650cfaf1b7a
fingerboy2019 <52920108+fingerboy2019@users.noreply.github.com>
2024-09-25 16:37:11 +0800

    Update SrcJuSet.js

--- Ju/SrcJuSet.js
+++ Ju/SrcJuSet.js
@@ -1,5 +1,5 @@
 ////本代码仅用于个人学习，请勿用于其他作用，下载后请24小时内删除，代码虽然是公开学习的，但请尊重作者，应留下说明
-let publicfile;
+//let publicfile;
 try{
     publicfile = config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcJuPublic.js';
 }catch(e){
